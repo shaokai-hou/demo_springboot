f0fc9c5ab3eb70e95e50aa1060602192fedc747d
shaokai-hou <17392652059@163.com>
2022-04-28 11:53:18 +0800

    jpa-init

---
+++ demo_jpa/src/main/java/com/haohao/jpa/JpaApplication.java
@@ -0,0 +1,14 @@
+package com.haohao.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haohao
+ */
+@SpringBootApplication
+public class JpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+}
