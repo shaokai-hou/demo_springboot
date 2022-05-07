06179880bd59cd9fb18d310dbd1fd84c4794566c
shaokai-hou <17392652059@163.com>
2022-05-08 02:39:16 +0800

    上传文件

--- demo_minio/src/main/java/com/haohao/minio/config/WebMvcConfig.java
+++ demo_minio/src/main/java/com/haohao/minio/config/WebMvcConfig.java
@@ -1,12 +1,10 @@
-package com.haohao.web.socket.config;
+package com.haohao.minio.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * WebMvcConfig
- *
  * @author haohao
  */
 @Configuration
@@ -14,6 +12,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("/index");
+        registry.addViewController("/file").setViewName("/index");
     }
 }
--- demo_minio/src/main/java/com/haohao/minio/service/impl/FileServiceImpl.java
+++ demo_minio/src/main/java/com/haohao/minio/service/impl/FileServiceImpl.java
@@ -1,21 +1,22 @@
 package com.haohao.minio.service.impl;
 
+import cn.hutool.core.codec.Base64;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileTypeUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileNameUtil;
 import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.IdUtil;
 import com.haohao.minio.config.MinioConfig;
 import com.haohao.minio.service.IFileService;
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.RemoveObjectArgs;
+import io.minio.*;
+import io.minio.messages.Bucket;
 import io.minio.messages.DeleteObject;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Date;
@@ -42,7 +43,24 @@ public class FileServiceImpl implements IFileService {
      */
     @Override
     public String uploadFile(MultipartFile file) throws Exception {
-        String fileName = file.getOriginalFilename();
+        // 获取文件名
+        String name = file.getOriginalFilename();
+        // 获取文件名称、文件后缀
+        assert name != null;
+        String prefix = name.substring(name.lastIndexOf("."));
+        // 获取年月日
+        int year = DateUtil.date().year();
+        int month = DateUtil.date().monthBaseOne();
+        int day = DateUtil.date().dayOfMonth();
+        year = 2021;
+        day = 9;
+        // 获取基础路径
+        String basePath = year + "年" + File.separator + month + "月" + File.separator + day + "日" + File.separator;
+        // 创建目录
+        createDir(minioConfig.getBucketName(), basePath);
+        // 生产随机名称
+        String fileName = basePath + IdUtil.simpleUUID() + prefix;
+        // 构建参数、上传
         PutObjectArgs args = PutObjectArgs.builder()
                 .bucket(minioConfig.getBucketName())
                 .object(fileName)
@@ -87,5 +105,40 @@ public class FileServiceImpl implements IFileService {
         for (String fileName : fileNames) {
             deleteFile(fileName);
         }
+    }
+
+    /**
+     * 判断Bucket是否存在
+     *
+     * @return true：存在，false：不存在
+     * @throws Exception Exception
+     */
+    private boolean isBucket(String bucketName) throws Exception {
+        return minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+    }
+
+    /**
+     * 获得所有Bucket列表
+     *
+     * @return Bucket列表
+     * @throws Exception Exception
+     */
+    public List<Bucket> getBuckets() throws Exception {
+        return minioClient.listBuckets();
+    }
+
+    /**
+     * 创建文件夹或目录
+     *
+     * @param bucketName 存储桶
+     * @param objectName 目录路径
+     */
+    public ObjectWriteResponse createDir(String bucketName, String objectName) throws Exception {
+        return minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .stream(new ByteArrayInputStream(new byte[]{}), 0, -1)
+                        .build());
     }
 }
